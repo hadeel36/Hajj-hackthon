bab485467f90a7c0867168fe382e9c7f5683914d
Malak Abu Hammad <malak.abuhammad@gmail.com>
2018-08-02 19:53:11 +0300

    create account access

--- back-end/code/hajj/haj/protected/controllers/GroupSupervisorController.php
+++ back-end/code/hajj/haj/protected/controllers/GroupSupervisorController.php
@@ -41,7 +41,7 @@ class GroupSupervisorController extends Controller
     {
         return array(
             array('allow',  // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view'),
+                'actions' => array('index', 'view', 'createAccount'),
                 'users' => array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
